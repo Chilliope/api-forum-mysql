d805e88ac4bd687a9006e328ee21769695d142fc
Erlang Andriyanputra <chilliope48@gmail.com>
2024-12-07 11:21:26 +0800

    config awal

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const express = require('express')
+
+const middlewareLogRequest = require('./middleware/logs')
+
+const app = express(middlewareLogRequest)
+
+app.use(middlewareLogRequest)
+
+app.listen(PORT, () => {
+    console.log(`server berjalan di port ${PORT}`)
+})
